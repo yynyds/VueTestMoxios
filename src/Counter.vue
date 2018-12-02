64a2434ec9a474d2439db1e3a1d146b94999f2d8
yynyds <yarik1120@gmail.com>
2018-12-02 21:27:36 +0200

    first commit!

---
+++ src/Counter.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+	  {{ count }}
+	  <button @click="increment">Увеличить</button>
+	</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      count: 0
+    }
+  },
+
+  methods: {
+    increment () {
+      this.count++
+    }
+  }
+}
+</script>
